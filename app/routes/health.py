cb8680e13c6cbf0cd9ba28ee4e28665b0f11f822
Victor Gonzalez <vemiliogp@gmail.com>
2025-11-16 17:04:03 +0100

    feat: update health check route to use a cleaner endpoint and add status code

--- app/routes/health.py
+++ app/routes/health.py
@@ -5,12 +5,12 @@ from fastapi import APIRouter
 from app.controllers.health import HealthController
 from app.services.health import HealthService
 
-router = APIRouter()
+router = APIRouter(prefix="/health", tags=["Health"])
 
 health_controller = HealthController(health_service=HealthService())
 
 
-@router.get("/health", tags=["Health"])
+@router.get("", status_code=200)
 async def health_check():
     """
     Health check endpoint.
