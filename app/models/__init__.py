cc59fdbdbb4f750c524bf10d6c43dad4e1b10690
Victor Gonzalez <vemiliogp@gmail.com>
2025-11-17 14:54:47 +0100

    feat: update conversation model to allow nullable title and refine imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,8 @@
 """Models registration."""
 
-from app.models.user import User
 from app.models.connection import Connection
 from app.models.conversation import Conversation
 from app.models.message import Message
+from app.models.user import User
 
 __all__ = ["User", "Connection", "Conversation", "Message"]
--- app/models/conversation.py
+++ app/models/conversation.py
@@ -3,6 +3,7 @@
 from tortoise import fields
 from tortoise.models import Model
 
+from app.models.connection import Connection
 from app.models.user import User
 
 
@@ -10,11 +11,17 @@ class Conversation(Model):
     """Conversation model representing a conversation in the system."""
 
     id = fields.IntField(pk=True)
-    title = fields.CharField(max_length=50, unique=True)
+    title = fields.CharField(max_length=50, null=True)
     context = fields.TextField(null=True)
 
     user: fields.ForeignKeyRelation[User] = fields.ForeignKeyField(
         "models.User", related_name="conversations", on_delete=fields.CASCADE
+    )
+    connection: fields.ForeignKeyNullableRelation[Connection] = fields.ForeignKeyField(
+        "models.Connection",
+        related_name="conversations",
+        null=True,
+        on_delete=fields.SET_NULL,
     )
 
     created_at = fields.DatetimeField(auto_now_add=True)
