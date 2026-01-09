b1d95bd469c3b3939bd22d6398ea0ac37f6679b1
Victor Gonzalez <vemiliogp@gmail.com>
2026-01-09 23:37:46 +0100

    feat: implement AgentInsight class and integrate with InsightService for generating insights

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,8 +2,8 @@
 
 from app.models.connection import Connection
 from app.models.conversation import Conversation
+from app.models.insight import Insight
 from app.models.message import Message
 from app.models.user import User
-from app.models.insight import Insight
 
 __all__ = ["User", "Connection", "Conversation", "Message", "Insight"]
--- app/services/insight.py
+++ app/services/insight.py
@@ -4,13 +4,17 @@ from dataclasses import dataclass
 
 from fastapi import HTTPException, status
 
+from app.agent.agent_insight import AgentInsight
 from app.dtos.insight import (
     CreateInsightsRequest,
     CreateInsightsResponse,
     GetInsightsResponse,
     InsightData,
 )
+from app.models.connection import Connection
 from app.models.insight import Insight
+from app.services.database import DatabaseService
+from app.utils.encrypt import Encrypt
 
 
 @dataclass
@@ -23,7 +27,34 @@ class InsightService:
         """
         Create new insights.
         """
-        pass
+        try:
+            connection = await Connection.get_or_none(
+                id=payload.connection_id, user_id=user_id
+            )
+            if not connection:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="Connection not found",
+                )
+
+            connection_url = Encrypt.decrypt(connection.encrypted_url)
+
+            tables = DatabaseService.get_tables(connection_url)
+
+            agent = AgentInsight(
+                model="deepseek/r1",
+                connection_url=connection_url,
+                context=payload.context,
+                tables=str(tables),
+            )
+
+            response = agent.run(num_insights=payload.count)
+
+            print("Agent Response:", response)
+
+            return response
+        except Exception as e:
+            raise e
 
     async def get_insights(self, user_id: str) -> GetInsightsResponse:
         """
